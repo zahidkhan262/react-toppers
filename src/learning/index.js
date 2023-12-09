5837e8760d389839d30d9ec4585470e3bda6c5d2
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2023-12-09 19:23:46 +0530

    Update index.js

--- src/learning/index.js
+++ src/learning/index.js
@@ -1,3 +1,126 @@
+
+
+// javacript training 08 december 2023
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+
+<body>
+    <div id="js">
+        <h1>Javascript </h1>
+    </div>
+
+    <script>
+        "use strict"
+        // var jsId = document.getElementById("js").innerHTML = "hello js"
+        // console.log(jsId, "jssss")
+
+        // var , let, const variable declaration
+        // var is  global and functional scope.
+
+        // let and const are block scope
+        // let cannt  re-declare can reasign
+
+        // function person() {
+        // }
+        // person();
+        // let newData;
+        // newData = 12
+        // if (true) {
+        //     var greet = "hello"
+        // }
+
+        // console.log(greet, "12")
+
+        // // const greet = 0;
+        // // const greet = 15
+
+        // const data = [1, 2, 3, 4, 5, 5]
+
+        /// types of function
+
+        // regular function
+        // arrow function
+        // iife function
+        //  anonymous function
+        // pure function
+        // hof higher order function
+        //
+
+        // syntaxt
+        // function test(){
+
+        // }
+
+        // const greet1 = () =>{
+
+        // }
+
+
+        // (function(){
+
+        // })()
+        // const n =12;
+
+
+        // function test(arr,fn){
+        //     return arr.map( (ele,index)=> fn(ele) )
+        // }
+        // const data = [1, 2, 3, 4, 5, 5]
+
+
+        // const double = (num) =>{
+        //     return num *2
+        // }
+
+        // test(data,double)
+
+
+
+
+    </script>
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // diff b/w function and method
 // const greet = () => {
 //   return `hello flash`
